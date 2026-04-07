0adae989bdbb755e94c0544f6f7f6e537e9368c3
Veronika Lykova <veronika.lykova.18@gmail.com>
2026-04-07 21:14:24 +0200

    Lesson 7 tasks

---
+++ homework/veronika_lykova/Lesson_7/task3.py
@@ -0,0 +1,14 @@
+string1 = 'результат операции: 42'
+string2 = 'результат операции: 514'
+string3 = 'результат работы программы: 9'
+string4 = 'результат: 2'
+
+def plus_ten(string):
+    find_number_position = string.index(':') + 2
+    number = int(string[find_number_position:]) + 10
+    print(number)
+
+plus_ten(string1)
+plus_ten(string2)
+plus_ten(string3)
+plus_ten(string4)
