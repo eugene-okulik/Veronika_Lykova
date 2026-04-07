e6e5d2b24662cf678a5df59bb21451c06c493ee1
Veronika Lykova <veronika.lykova.18@gmail.com>
2026-04-07 21:17:21 +0200

    Lesson 7 tasks fixed linter

--- homework/veronika_lykova/Lesson_7/task3.py
+++ homework/veronika_lykova/Lesson_7/task3.py
@@ -3,10 +3,12 @@ string2 = 'результат операции: 514'
 string3 = 'результат работы программы: 9'
 string4 = 'результат: 2'
 
+
 def plus_ten(string):
     find_number_position = string.index(':') + 2
     number = int(string[find_number_position:]) + 10
     print(number)
+
 
 plus_ten(string1)
 plus_ten(string2)
